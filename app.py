674a980b6aaa29778c29d94bc54b4aaf94106757
Sasindu Nimsara <sasinduwanninayake@gmail.com>
2026-09-19 23:15:22 +0530

    My Test Run

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="Hello from CSI2113 DevOps Practical! My Name Is Sasindu Nimsara - 8003369"), 200
+    return jsonify(message="Hello from CSI2113 DevOps Practical! My Name Is Sasindu Nimsara - 8003369.. This is my test run"), 200
 
 @app.route('/health')
 def health():
